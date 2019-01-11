96747a54be208d3e0305fe3c8ef53580b61c9ee2
cnsaavedra <christian-saavedra@hotmail.com>
2019-01-10 19:17:55 -0500

    Updates

---
+++ precache-manifest.db91c3540cca2c48330d04935ffc17db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cnsaavedra.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f76fb0f8cc84ba1fd212",
+    "url": "/cnsaavedra.github.io/static/js/runtime~main.f76fb0f8.js"
+  },
+  {
+    "revision": "c4ec95673c88799e0700",
+    "url": "/cnsaavedra.github.io/static/js/main.c4ec9567.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/cnsaavedra.github.io/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c4ec95673c88799e0700",
+    "url": "/cnsaavedra.github.io/static/css/main.47a3fb9d.chunk.css"
+  },
+  {
+    "revision": "7ba5b101e87b2520056c13969ed5b354",
+    "url": "/cnsaavedra.github.io/index.html"
+  }
+];
